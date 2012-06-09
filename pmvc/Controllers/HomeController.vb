5de08c91e2da3d986e793cd1ca3f8b585038d9fa
Axxis QA <jam@axxis-systems.com>
2012-06-09 12:32:34 -0500

    otro cambio
    sdsdsd

--- pmvc/Controllers/HomeController.vb
+++ pmvc/Controllers/HomeController.vb
@@ -16,6 +16,10 @@
     Function Contact() As ActionResult
         ViewData("Message") = "Your quintessential contact page."
         'cambio hecho por QA sobre rama desarrollo
+
+
+
+        'este es un cambio hecho por otra persona!!!!!!!!!!!!!!!!!!!
         Return View()
     End Function
 End Class
